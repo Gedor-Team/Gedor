658f044b03968bded52ec7908c9549815d8531d2
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2024-12-06 18:49:50 +0700

    fix API docs title name and description

--- cc/backend/src/index.js
+++ cc/backend/src/index.js
@@ -10,9 +10,9 @@ const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Complaint System API",
+      title: "Gedor API",
       version: "1.0.0",
-      description: "API documentation for the Complaint System",
+      description: "API documentation for the Gedor App",
     },
     servers: [
       {
