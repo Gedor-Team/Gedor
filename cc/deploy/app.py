158a4d085760c4e1e0d37d2535aa8496f3b3c711
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2024-12-11 14:55:46 +0700

    test workflow

--- cc/deploy/app.py
+++ cc/deploy/app.py
@@ -137,7 +137,7 @@ class ComplaintPrediction(Resource):
 
             input_tensor = np.array([preprocessed_complaint])
 
-            # Mock prediction for exampless
+            # Mock prediction for examplesss
             probability = complaint_model.predict(input_tensor).tolist()
             predictions = [pred >= 0.4 for pred in probability[0]]
 
