9d4024fadc22f4a8fd9a7fdda2e43d7def1bd370
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2024-12-11 14:10:03 +0700

    test workflow

--- cc/deploy/app.py
+++ cc/deploy/app.py
@@ -110,7 +110,7 @@ def preprocess_complaint(complaint):
     tokenized_complaint = tokenize_complaint(cleaned_complaint)
     return tokenized_complaint
 
-# Define resources
+# Define resourcesss
 @ns.route('/complaint')
 class ComplaintPrediction(Resource):
     @ns.expect(complaint_input_model)
